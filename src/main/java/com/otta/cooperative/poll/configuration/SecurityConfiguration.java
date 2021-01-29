a169e69555ed555568b28aff96fda474ad536e17
Guilherme Otta <guilhermeotta@gmail.com>
2021-01-29 16:25:05 -0300

    feat: Adiciona esqueleto da classe de configuração do Spring Security.

---
+++ src/main/java/com/otta/cooperative/poll/configuration/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.otta.cooperative.poll.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@ComponentScan
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    //private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    //private final MyUserDetailsService jwtUserDetailsService;
+    //private final JwtRequestFilter jwtRequestFilter;
+
+    /*public SecurityConfiguration(JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            MyUserDetailsService jwtUserDetailsService, JwtRequestFilter jwtRequestFilter) {
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+
+    }*/
+
+    /*@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                // Não cheque essas requisições
+                .authorizeRequests()
+                .antMatchers("/api/v1/authentication", "/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                        "/configuration/**", "/swagger-ui.html", "/webjars/**", "/h2-console/**", "/console/**")
+                .permitAll()
+                // Qualquer outra requisição deve ser checada
+                .anyRequest().authenticated().and().exceptionHandling();
+                //.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                //.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.headers().frameOptions().disable();
+    }
+}
